9a35d074aab55758c4b812a4b0e19a18561bd7e8
isaacmeedinaa <isaac.meedinaa@gmail.com>
2020-05-10 22:02:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let msgs_array = []
+
+    for (let index = 0; index < names.length; index++) {
+        msgs_array.push(`Thank you, ${names[index]}, for the wonderful ${event} gift!`)
+    }
+
+    return msgs_array;
+}
+
+// function countDown(num) {
+//     i = 0;
+
+//     while (i <= num) {
+//         num - i;
+//         i++;
+//     }
+// }
+
+function countDown(num) {
+    while (num > 0) {
+        console.log(num);
+        num -= 1;
+    }
+
+    console.log(num)
+}
